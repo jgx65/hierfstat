04e7cd2abce0e96e59429d21072bf5cce01e75aa
unknown <jgoudet@PC32819.ad.unil.ch>
2020-07-17 21:06:49 +0200

    new version 0.5-04, with new function read.VCF

--- R/readVCF.R
+++ R/readVCF.R
@@ -3,11 +3,12 @@
 #' Reads a \url{https://samtools.github.io/hts-specs/}{Variant Call Format (VCF)} file into a BED object,
 #' retaining bi-allelic SNPs only
 #'
-#' @usage read.VCF(fname,BiAlellic=TRUE,...)
+#' @usage read.VCF(fname,BiAllelic=TRUE,...)
 #' 
 #' @param fname VCF file name. The VCF file can be compressed (VCF.gz)
 #' @param BiAllelic  Logical. If TRUE, only bi-allelic SNPs are retained,
 #'    otherwise, all variant are kept
+#' @param ... other arguments to pass to the function    
 #'     
 #'    
 #' @return  A \code{\link[gaston]{bed.matrix}}   
@@ -24,7 +25,7 @@
 #' @export
 read.VCF<-function(fname,BiAllelic=TRUE,...){
   bed<-gaston::read.vcf(fname,...)
-  if(Biallelic){
+  if(BiAllelic){
   snps<-with(bed@snps,which(nchar(A1)==1 & nchar(A2)==1))
   bed<-bed[,snps]
   }
